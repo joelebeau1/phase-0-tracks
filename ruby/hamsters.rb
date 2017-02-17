ac60f04bad0e012a873e314c776a9f3c1ded4769
Joe LeBeau <joseph.p.lebeau@gmail.com>
2017-02-17 13:10:23 -0600

    user input prompts and variable storage added

---
+++ ruby/hamsters.rb
@@ -0,0 +1,29 @@
+puts "Welcome to the Hamster Adoption Mega-Center!"
+
+#Hamster's name 
+puts "1. What is your hamster's name?"
+hamster_name = gets.chomp
+
+#Volume level 1-10 
+
+puts "2. On a scale from 1-10, how loud is your hamster, with 1 being very quiet and 10 being extremely loud?"
+
+volume_level = gets.chomp
+
+#Fur color 
+
+puts "3. What color is your hamster's fur?"
+
+fur_color = gets.chomp
+
+# Good candidate for adoption? 
+
+puts "[Yes/No] is this hamster a good candidate for adoption?"
+
+adoption_candidate = gets.chomp
+
+#Estimated age 
+
+puts "What is your hamsters estimated age in years?"
+
+hamster_age = gets.chomp 
