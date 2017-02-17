96307b213fa4c343d67e4100a6cd5d761c736a0a
Joe LeBeau <joseph.p.lebeau@gmail.com>
2017-02-17 13:34:59 -0600

    Finish 4.3

--- ruby/hamsters.rb
+++ ruby/hamsters.rb
@@ -1,29 +1,44 @@
 puts "Welcome to the Hamster Adoption Mega-Center!"
 
 #Hamster's name 
+# String
 puts "1. What is your hamster's name?"
 hamster_name = gets.chomp
 
 #Volume level 1-10 
+# interger 
 
 puts "2. On a scale from 1-10, how loud is your hamster, with 1 being very quiet and 10 being extremely loud?"
 
 volume_level = gets.chomp
 
 #Fur color 
+#string 
 
 puts "3. What color is your hamster's fur?"
 
 fur_color = gets.chomp
 
 # Good candidate for adoption? 
+#boolean T/F 
 
-puts "[Yes/No] is this hamster a good candidate for adoption?"
+puts "4. [Yes/No] is this hamster a good candidate for adoption?"
 
 adoption_candidate = gets.chomp
 
 #Estimated age 
+#Interger and nil 
 
-puts "What is your hamsters estimated age in years?"
+puts "5. What is your hamsters estimated age in years?"
 
-hamster_age = gets.chomp 
+hamster_age = gets.chomp
+
+#print final results to console
+
+puts "Thank you for completing our intake form, view results below:"
+
+puts "Hamster name: #{hamster_name}"
+puts "Hamster's volume level: #{volume_level}"
+puts "Fur color: #{fur_color}"
+puts "Adoption candidate? #{adoption_candidate}"
+puts "Hamster estimated age: #{hamster_age}"
