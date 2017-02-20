75a010ae116689cfe4775846cd39256afe0733aa
Joe LeBeau <joseph.p.lebeau@gmail.com>
2017-02-20 09:55:18 -0600

    added spy interface

--- ruby/secret_agents.rb
+++ ruby/secret_agents.rb
@@ -11,6 +11,26 @@ end
 #Decrypt: reverse
 
 def decrypt(string)
-decrypted_string = string.tr('b', 'a').tr('c', 'b').tr('d', 'c').tr('e', 'd').tr('f', 'e').tr('g', 'f').tr('h', 'g').tr('i', 'h').tr('j', 'i').tr('k', 'j').tr('l', 'k').tr('m', 'l').tr('n', 'm').tr('o', 'n').tr('p', 'o').tr('q', 'p').tr('r', 'q').tr('s', 'r').tr('t', 's').tr('u', 't').tr('v', 'u').tr('w', 'v').tr('x', 'w').tr('y', 'x').tr('z', 'y').tr('a', 'z')
-p decrypted_string
+string = string.tr('b', 'a').tr('c', 'b').tr('d', 'c').tr('e', 'd').tr('f', 'e').tr('g', 'f').tr('h', 'g').tr('i', 'h').tr('j', 'i').tr('k', 'j').tr('l', 'k').tr('m', 'l').tr('n', 'm').tr('o', 'n').tr('p', 'o').tr('q', 'p').tr('r', 'q').tr('s', 'r').tr('t', 's').tr('u', 't').tr('v', 'u').tr('w', 'v').tr('x', 'w').tr('y', 'x').tr('z', 'y').tr('a', 'z')
 end 
+
+#Get user input: encrypt or decrypt
+puts "Hello comrade, do you need encryption or decryption services?"
+method_type = gets.chomp.downcase
+
+#Get user password
+puts "Enter your password now"
+password = "gets.chomp"
+
+#Add logic to choose method based on user input
+if method_type == "encryption";
+    encrypt(agent_password)
+    p agent_password
+end 
+if method_type == "decryption";
+    decrypt(agent_password)
+end 
+
+puts "Operation complete"
+
+#Perform operation and print results, then exits
