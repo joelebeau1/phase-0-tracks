8d65cd74d71657b3df85af9fdad9f56fc8d92b49
Joe LeBeau <joseph.p.lebeau@gmail.com>
2017-02-20 03:41:30 -0600

    add detection logic

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -16,58 +16,55 @@ real_age = 2017 - birth_year
 
 puts "[Y/N] I am interested in ordering garlic bread from the company cafeteria"
 garlic_preference = gets.chomp.upcase
-if garlic_preference == "Y"
-    eats_garlic = true
-elsif garlic_preference == "YES"
-  eats_garlic = true
-elsif garlic_preference == "N"
-    eats_garlic = false
-elsif garlic_preference == "NO"
-  eats_garlic = false
-end
 
 # If they want to enroll in company health insurance
 puts "[Y/N] Do you plan on enrolling in our company health insurance plan?"
 health_plan = gets.chomp.upcase
-if health_plan == "Y"
-    immortal_being = false
-elsif health_plan == "YES"
-  immortal_being = false
-elsif health_plan == "N"
-    immortal_being = true
-elsif health_plan == "NO"
-  immortal_being = true
+
+puts "SURVEY RESULTS:"
+
+# #Detection Logic
+unlikely_vampire = false
+possible_vampire = false
+probable_vampire = false
+definite_vampire = false
+
+if employee_age == real_age && garlic_preference == "Y";
+  
+  unlikely_vampire = true
+end
+  
+if ((garlic_preference == "N") && (health_plan == "N") && (real_age != employee_age));
+  
+  probable_vampire = true
+  
+elsif ((garlic_preference = "N" || health_plan = "N") && (real_age != employee_age));
+  
+  possible_vampire = true
+end 
+
+if employee_name == "DRAKE CULA" || employee_name == "TU FANG";
+  
+  definite_vampire = true
 end
 
-#Detection Logic
-if employee_age = real_age && eats_garlic = true
-    secret_vampire == "Probably not a vampire"
+#debugging block
+# p unlikely_vampire
+# p possible_vampire
+# p probable_vampire
+# p definite_vampire
 
-elsif real_age != employee_age && (eats_garlic = false || immortal_being = true)
-    secret_vampire == "Probably a vampire"
+if definite_vampire == true;
+  puts "Definitely a vampire!"
 
-elsif real_age != employee_age && eats_garlic = false && immortal_being = true
-    secret_vampire == "Almost certainly a vampire"
+elsif unlikely_vampire == true;
+  puts "Most likely not a vampire!"
+  
+elsif probable_vampire == true;
+  puts "Almost certainly a vampire!"
 
-elsif employee_name = "DRAKE CULA" 
-    secret_vampire == "Definitely a vampire, run away!"
-
- elsif employee_name = "TU FANG" 
-    secret_vampire == "Definitely a vampire, run away!"
-
-else
-    puts "Results inconclusive"
-
-
-
-    
-puts "Thank you for completing our survey, your results are displayed below:"
-puts "Name: #{employee_name.}"
-puts "Age: #{employee_age}"
-puts "Year of birth: #{birth_year}"
-puts "Garlic bread preference: #{garlic_preference}"
-puts "Enrolling in company health insurance: #{health_plan}"
-
-puts "END OF SURVEY: DO NOT READ BELOW THIS LINE!"
+elsif possible_vampire == true;
+  puts "Probably a vampire!"
+end 
 
 
