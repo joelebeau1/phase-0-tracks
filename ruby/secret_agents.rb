61015720e81dba866b56918d6f95a75473680758
Joe LeBeau <joseph.p.lebeau@gmail.com>
2017-02-20 09:41:47 -0600

    add decryption method

--- ruby/secret_agents.rb
+++ ruby/secret_agents.rb
@@ -1,15 +1,16 @@
 #Encrypt method: move each letter in a string one letter forward
 
-def encrypt(password)
+def encrypt(string)
   index =  0
-  while index < password.length
-    password[index] = password[index].next!
+  while index < string.length
+    string[index] = string[index].next!
     index += 1
   end
 end 
-    
-
-#1. Define method & parameter
-#2. Create +1 letter code
 
 #Decrypt: reverse
+
+def decrypt(string)
+decrypted_string = string.tr('b', 'a').tr('c', 'b').tr('d', 'c').tr('e', 'd').tr('f', 'e').tr('g', 'f').tr('h', 'g').tr('i', 'h').tr('j', 'i').tr('k', 'j').tr('l', 'k').tr('m', 'l').tr('n', 'm').tr('o', 'n').tr('p', 'o').tr('q', 'p').tr('r', 'q').tr('s', 'r').tr('t', 's').tr('u', 't').tr('v', 'u').tr('w', 'v').tr('x', 'w').tr('y', 'x').tr('z', 'y').tr('a', 'z')
+p decrypted_string
+end 
