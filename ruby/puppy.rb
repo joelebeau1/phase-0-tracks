2541f83a9b855129a54747749a5c083170a482fd
Joe LeBeau <joseph.p.lebeau@gmail.com>
2017-03-01 12:41:38 -0600

    finish 6.1

---
+++ ruby/puppy.rb
@@ -0,0 +1,18 @@
+# Puppy Class Design:
+
+# Class: Puppy
+
+# Characteristics-------------------
+# Size: small
+# Friendly: yes
+# Obedient: no
+# Energy (1-10): 10
+# Name: varies
+# Color: varies
+# Cuteness: 10
+
+# Behavior------------------------
+# Play
+# Eat
+# Nap
+# Repeat
