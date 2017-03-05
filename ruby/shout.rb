ed1a5839014409330aec7b3c9b55a8898dea4929
Joe LeBeau <joseph.p.lebeau@gmail.com>
2017-03-05 16:12:22 -0600

    convert shout module from standalone to mix-in

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,15 +1,35 @@
+# # Release 1:
+# module Shout
+
+#   def self.yell_angrily(words)
+#     words + "!!!" + " :("
+#   end
+
+#   def self.yelling_happily(words)
+#     words + "!!!" + " :)" + " I'm so happy!"
+#   end 
+
+# end 
+
+# # Driver Code
+# p Shout.yell_angrily("You can't ground me, I quit")
+# p Shout.yelling_happily("I'm not grounded anymore")
+
 module Shout
 
-  def self.yell_angrily(words)
-    words + "!!!" + " :("
-  end
+    def yell_angrily(words)
 
-  def self.yelling_happily(words)
-    words + "!!!" + " :)" + " I'm so happy!"
-  end 
+        words + "!!!" + " :("
+    end 
+
+    def yell_happily(words)
+
+        words + "!" + " :)" + "I'm so happy!"
+    end
+
+    def startled_yell(words)
+
+        "AHH! " + words + " That startled me!"
+    end
 
 end 
-
-# Driver Code
-p Shout.yell_angrily("You can't ground me, I quit")
-p Shout.yelling_happily("I'm not grounded anymore")
