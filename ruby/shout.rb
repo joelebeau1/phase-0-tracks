aec6e88bcc9aba8f655ae2f71c7a356fc3630ae2
Joe LeBeau <joseph.p.lebeau@gmail.com>
2017-03-05 16:20:27 -0600

    completed 6.4

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,4 +1,4 @@
-# # Release 1:
+# # Release 1: Shout module as Standalone
 # module Shout
 
 #   def self.yell_angrily(words)
@@ -15,31 +15,38 @@
 # p Shout.yell_angrily("You can't ground me, I quit")
 # p Shout.yelling_happily("I'm not grounded anymore")
 
+# Release 3: Shout module as mixin
 module Shout
 
     def yell_angrily(words)
+        puts words + "!!!" + " :("
 
-        words + "!!!" + " :("
     end 
 
     def yell_happily(words)
+        puts words + "!" + " :)" + " I'm so happy!"
 
-        words + "!" + " :)" + "I'm so happy!"
     end
-
-    def startled_yell(words)
-
-        "AHH! " + words + " That startled me!"
-    end
-
 end 
 
 class Toddler
-
   include Shout 
+
 end 
 
 class Teenager
-
   include Shout
+
 end
+
+# Driver code
+timmy = Toddler.new
+timmy.yell_angrily("My tooth just fell out")
+timmy.yell_happily("I woke up with money under my pillow")
+
+samantha = Teenager.new
+samantha.yell_angrily("This house is like a prison, worst parents ever")
+samantha.yell_happily("Thanks for the new car Mom and Dad")
+
+
+
