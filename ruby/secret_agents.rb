73ba56645e7fabc51833dc60c6287b4748864581
Joe LeBeau <joseph.p.lebeau@gmail.com>
2017-03-06 06:33:47 -0600

    fixed encrypt/decrypt methods, improved UI

--- ruby/secret_agents.rb
+++ ruby/secret_agents.rb
@@ -1,36 +1,53 @@
 #Encrypt method: move each letter in a string one letter forward
 
-def encrypt(string)
+def encrypt(password)
   index =  0
-  while index < string.length
-    string[index] = string[index].next!
+  while index < password.length
+    password[index] = password[index].next!
     index += 1
   end
+  password
 end 
 
 #Decrypt: reverse
 
 def decrypt(string)
-string = string.tr('b', 'a').tr('c', 'b').tr('d', 'c').tr('e', 'd').tr('f', 'e').tr('g', 'f').tr('h', 'g').tr('i', 'h').tr('j', 'i').tr('k', 'j').tr('l', 'k').tr('m', 'l').tr('n', 'm').tr('o', 'n').tr('p', 'o').tr('q', 'p').tr('r', 'q').tr('s', 'r').tr('t', 's').tr('u', 't').tr('v', 'u').tr('w', 'v').tr('x', 'w').tr('y', 'x').tr('z', 'y').tr('a', 'z')
+decrypted_string = string.tr('bcdefghijklmnopqrstuvwxyza', 'abcdefghijklmnopqrstuvwxyz')
+decrypted_string
 end 
 
 #Get user input: encrypt or decrypt
-puts "Hello comrade, do you need encryption or decryption services?"
+
+more_passwords = "yes"
+
+until more_passwords == "no"
+
+puts "Hello comrade, do you need to encrypt or decrypt a password?"
 method_type = gets.chomp.downcase
 
+if method_type == "encrypt"
+    puts "Enter password now"
+    agent_password = gets.chomp.downcase
+    puts "Encrypted password is #{encrypt(agent_password)}"
+
+elsif method_type == "decrypt"
+    puts "Enter password now"
+    agent_password = gets.chomp.downcase
+    puts "Decrypted password is #{decrypt(agent_password)}"
+else false
+end
+
+puts "Do you have another password to encrypt or decrypt? [yes/no]"
+more_passwords = gets.chomp.downcase
+
+end
+
+puts "Operation complete: this operation never happened"
+
+
 #Get user password
-puts "Enter your password now"
-password = "gets.chomp"
 
 #Add logic to choose method based on user input
-if method_type == "encryption";
-    encrypt(agent_password)
-    p agent_password
-end 
-if method_type == "decryption";
-    decrypt(agent_password)
-end 
 
-puts "Operation complete"
 
 #Perform operation and print results, then exits
