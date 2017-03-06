8135c6ac60600c280a638a8e044030a563473e80
Joe LeBeau <joseph.p.lebeau@gmail.com>
2017-03-06 03:16:16 -0600

    finish 6.6

--- ruby/game_class/game.rb
+++ ruby/game_class/game.rb
@@ -32,10 +32,12 @@ def initialize
   @correct_guess = false
 end
 
+# Set a limit on the number of guesses based on secret word length: 2 guesses per letter
 def calculate_limit(word)
   @guess_limit = (word.length * 2)
 end
 
+# Check to see if the secret word contains a guessed letter 
 def check_guess(word, guess)
   if word.include? guess
     @correct_guess = true
@@ -45,11 +47,13 @@ def check_guess(word, guess)
   end
 end
 
+# Establish the starting word status feedback message to a string with one "-" to represent each letter in secret word
 def set_progress_msg(word)
   @progress_message = ("-" * word.length)
   @progress_message
 end
 
+# Update feedback message to include correctly guessed letters in place of "-"
 def track_progress(word, guess, correct_guess, progress_message)
     
   if @correct_guess == true
@@ -70,6 +74,7 @@ def track_progress(word, guess, correct_guess, progress_message)
   end
 end
 
+# Check for victory conditions, in which the status of the guessed word equals the secret word
 def check_for_victory(word, new_progress_message)
   if word == @new_progress_message
     @victory = true
@@ -98,29 +103,35 @@ puts ""
 puts "The secret word is #{game.set_progress_msg(@secret_word)}"
 puts ""
 
+# Establish the length of the game to be the guess limit
 until @guess_count == @guess_limit
   @guess_count += 1
 
   puts "Player 2, enter a letter to guess, or type 'qq to quit"
   @guessed_letter = gets.chomp.downcase
 
+# Allow user to quit early
   break if @guessed_letter == "qq"
 
   puts ""
 
+# Run looping guess checks and updating word status feedback message
   game.check_guess(@secret_word, @guessed_letter)
 
   game.track_progress(@secret_word, @guessed_letter, @correct_guess, @progress_message)
 
+# Break game loop when victory conditions are met and print congrats message
   if game.check_for_victory(@secret_word, @new_progress_message) == true
     puts "Congratulations! You guessed the word! The word is #{@secret_word}"
     break
   end
 
+#Update user each time they use a guess with their remaining number of guesses
   puts "You have #{@guess_limit - @guess_count} guesses left"
   puts ""
 end 
 
+# If all guesses are used by user and victory conditions aren't met, print defeat message
 if !game.check_for_victory(@secret_word, @new_progress_message)
   puts "Oh no, you couldn't guess the word! So sad :("
 end
