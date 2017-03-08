27b1da6380c63436b9ddea10b9c2bb23c6a2a0c6
Joe LeBeau <joseph.p.lebeau@gmail.com>
2017-03-08 15:16:02 -0600

    Release 0

---
+++ js/data_structures.js
@@ -0,0 +1,8 @@
+var colors = ['blue', 'red', 'green', 'yellow'];
+var names = ['Ed', 'Eddie', 'Mr. Ed', 'Edd'];
+
+colors.push('purple');
+names.push('Ted');
+
+console.log(colors);
+console.log(names);
