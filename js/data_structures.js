73d35a13bd8fc7599b04b78a1cf42f94702b3ab9
Joe LeBeau <joseph.p.lebeau@gmail.com>
2017-03-08 15:40:28 -0600

    Release 2

--- js/data_structures.js
+++ js/data_structures.js
@@ -14,3 +14,26 @@ for(i = 0; i < names.length; i++) {
 };
 
 console.log(horses);
+
+function Car(make, model, year, color) {
+    console.log('A new car:', this);
+    this.make = make;
+    this.model = model;
+    this.year = year;
+    this.color = color;
+
+    this.revEngine = function() {console.log('VROOM VROOM');};
+    console.log('Enjoy your new car!');
+}
+
+var newCar = new Car('Chevy', 'Malibu', 2016, 'red');
+console.log(newCar);
+newCar.revEngine();
+
+var newerCar = new Car('Chevy', 'Malibu', 2017, 'yellow');
+console.log(newerCar);
+newerCar.revEngine();
+
+var newestCar = new Car('Tesla', 'Flying Car', 2020, 'black');
+console.log(newestCar);
+newestCar.revEngine();
