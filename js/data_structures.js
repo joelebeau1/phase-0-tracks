7a768c7c283cf01b8c7cb8fddc79b5805280e59e
Joe LeBeau <joseph.p.lebeau@gmail.com>
2017-03-08 15:25:43 -0600

    Release 1

--- js/data_structures.js
+++ js/data_structures.js
@@ -6,3 +6,11 @@ names.push('Ted');
 
 console.log(colors);
 console.log(names);
+
+var horses = {};
+
+for(i = 0; i < names.length; i++) {
+    horses[names[i]] = colors[i];
+};
+
+console.log(horses);
