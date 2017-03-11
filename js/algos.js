7d2a70fcdc59b8737d438732507a370753221855
Joe LeBeau <joseph.p.lebeau@gmail.com>
2017-03-11 14:35:08 -0600

    add structure for matching function

--- js/algos.js
+++ js/algos.js
@@ -1,6 +1,5 @@
 // Write function that takes an array of words or phrases and returns the longest
 
-
 function findLongest(array) {
  var sorted = array.sort(function (a, b) {
     return a.length < b.length;
@@ -8,7 +7,18 @@ function findLongest(array) {
  return sorted[0];
 };
 
+// Function that takes two objects (hashes) and checks to see if the objects share at least
+// one key-value pair, returning true if >1 match is found and false if no matches found
 
+function findMatch(hash1, hash2) {
+
+    
+}
+
+
+// DRIVER CODE
 var words = ["long", "longer", "longest", "longest and then some"];
-
 console.log(findLongest(words));
+
+var colors = ['red', 'blue', 'green', 'iridescent aqua'];
+console.log(findLongest(colors));
