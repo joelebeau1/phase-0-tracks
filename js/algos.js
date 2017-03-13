53a5f64687aa19c60c9161a2c120d3c43560d18a
Joe LeBeau <joseph.p.lebeau@gmail.com>
2017-03-13 06:20:32 -0500

    add function to find matching name-vaulue pairs in objects

--- js/algos.js
+++ js/algos.js
@@ -7,12 +7,16 @@ function findLongest(array) {
  return sorted[0];
 };
 
-// Function that takes two objects (hashes) and checks to see if the objects share at least
+// Function that takes two objects and checks to see if the objects share at least
 // one key-value pair, returning true if >1 match is found and false if no matches found
 
-function findMatch(hash1, hash2) {
-
+function findMatch(object1, object2) {
+  return Object.keys(object1).reduce(function(map, k) {
+    if (object1[k] == object2[k]) map[k] = object2[k];
+    return map;
     
+  }, {});
+
 }
 
 
@@ -22,3 +26,7 @@ console.log(findLongest(words));
 
 var colors = ['red', 'blue', 'green', 'iridescent aqua'];
 console.log(findLongest(colors));
+
+var me = {name:"Joe", age:25, residence:"Chicago"};
+var futureMe = {name:"Joey", age:26, residence:"Unknown"};
+console.log(findMatch(me, futureMe));
