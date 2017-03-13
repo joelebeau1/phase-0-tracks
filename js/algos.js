33c25af549c662d52b19db27ed71d6db87c687e5
Joe LeBeau <joseph.p.lebeau@gmail.com>
2017-03-13 06:54:43 -0500

    add random test data function & driver code

--- js/algos.js
+++ js/algos.js
@@ -11,14 +11,25 @@ function findLongest(array) {
 // one key-value pair, returning true if >1 match is found and false if no matches found
 
 function findMatch(object1, object2) {
-  return Object.keys(object1).reduce(function(map, k) {
-    if (object1[k] == object2[k]) map[k] = object2[k];
-    return map;
-    
-  }, {});
+  return Object.keys(object1).reduce(function(a, b) {
+    if (object1[b] == object2[b]) a[b] = object2[b];
+    return a;
 
+  }, {});
 }
 
+// Function that takes integer for length, returns array of random strings of given length
+
+function randomStrings(x) {
+    var string = "";
+    var possible = "abcdefghijklmnopqrstuvwxyz";
+    var randArray = []
+    for (i = 0; i < x; i++) {
+          string += possible.charAt(Math.floor(Math.random() * possible.length));
+          randArray.push(string);
+    }
+    return randArray;
+}
 
 // DRIVER CODE
 var words = ["long", "longer", "longest", "longest and then some"];
@@ -28,5 +39,10 @@ var colors = ['red', 'blue', 'green', 'iridescent aqua'];
 console.log(findLongest(colors));
 
 var me = {name:"Joe", age:25, residence:"Chicago"};
-var futureMe = {name:"Joey", age:26, residence:"Unknown"};
+var futureMe = {name:"Joe", age:26, residence:"Unknown"};
 console.log(findMatch(me, futureMe));
+
+array = randomStrings(10);
+console.log(array);
+console.log(findLongest(array));
+
