9d84169b144ee98ac799fc5868110bec3d1d3538
Joe LeBeau <joseph.p.lebeau@gmail.com>
2017-03-21 14:53:49 -0500

    add event listeners

--- web_dev/client_side_js/script.js
+++ web_dev/client_side_js/script.js
@@ -1,2 +1,22 @@
-var first_line = document.getElementByIdName("first-line");
+var first_line = document.getElementById("first-line");
 first_line.style.border = "3px solid black";
+
+var barcelona = document.getElementById("barcelona");
+
+var header = document.getElementById("header");
+
+var mountain_bike = document.getElementById("mountain-bike");
+
+function addRedBorder(event) {
+    console.log("we're adding a red border");
+    event.target.style.border = "5px solid red";
+};
+
+function addYellowBorder(event) {
+    console.log("we're adding a yellow border");
+    event.target.style.border = "5px solid yellow";
+};
+
+header.addEventListener("mouseover", addRedBorder);
+
+barcelona.addEventListener("click", addYellowBorder);
