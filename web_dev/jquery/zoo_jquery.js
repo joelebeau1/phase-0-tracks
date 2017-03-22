0d2018306953cb54a4ffef2d7031aceecebacbb6
Joe LeBeau <joseph.p.lebeau@gmail.com>
2017-03-21 20:04:14 -0500

    add animations for friendly and bananas class

--- web_dev/jquery/zoo_jquery.js
+++ web_dev/jquery/zoo_jquery.js
@@ -17,11 +17,23 @@ $(document).ready(function() {
             $(this).animate({height: '-=25px', width: '-=25px'}, "slow");
         },
         mouseleave: function(){
-            $(this).animate({height: '300px', width: '300px'}, 10000);
+            $(this).animate({height: '300px', width: '300px'}, 5000);
         }
     });
 
-    
+    $(".friendly").on({
+        click: function(){
+            $(this).animate({height: '+=25px', width: '+=25px'}, "slow");
+        },
+        mouseleave: function(){
+            $(this).animate({height: '300px', width: '300px'}, 5000);
+        }
+    });
 
-
+    $(".bananas").click(function(){
+        $(this).animate({height: '500px'}, "fast");
+        $(this).animate({width: '500px'}, "fast");
+        $(this).animate({height: '300px'}, "fast");
+        $(this).animate({width: '300px'}, "fast");
+    });
 });
